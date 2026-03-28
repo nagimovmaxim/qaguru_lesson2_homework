847ab3230f61396f80bd375ab701176bb38725f6
NagimovMaximVasilevich <maxim.nagimov@gmail.com>
2026-03-28 20:40:29 +0500

    Replace hardcoded string with constant variable

--- src/main/java/com/nagimovmaxim/Main.java
+++ src/main/java/com/nagimovmaxim/Main.java
@@ -3,10 +3,12 @@ package com.nagimovmaxim;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final String s = "PIPIPIPIPIPIPIPIPI";
+    
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("KEKEKEKEKKEKEKEKEKEKEK!");
+        System.out.printf(s);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
